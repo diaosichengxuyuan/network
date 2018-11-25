a8b09c9a3b77031d23dc88ac1b60435421bee965
“liuhaipeng” <>
2018-11-26 00:41:32 +0800

    开源协议示例

--- com.diaosichengxuyuan.network.parent/com.diaosichengxuyuan.network.protocal/src/main/java/com/diaosichengxuyuan/network/protocal/modbus/ModbusClient.java
+++ com.diaosichengxuyuan.network.parent/com.diaosichengxuyuan.network.protocal/src/main/java/com/diaosichengxuyuan/network/protocal/modbus/ModbusClient.java
@@ -1,9 +1,36 @@
 package com.diaosichengxuyuan.network.protocal.modbus;
 
+import com.digitalpetri.modbus.master.ModbusTcpMaster;
+import com.digitalpetri.modbus.master.ModbusTcpMasterConfig;
+import com.digitalpetri.modbus.requests.ReadHoldingRegistersRequest;
+import com.digitalpetri.modbus.responses.ReadHoldingRegistersResponse;
+import io.netty.buffer.ByteBuf;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
 /**
  * @author liuhaipeng
  * @date 2018/11/18
  */
 public class ModbusClient {
+    private final ModbusTcpMaster master;
 
+    public ModbusClient() {
+        master = new ModbusTcpMaster(new ModbusTcpMasterConfig.Builder("localhost")
+            .setPort(50200).build());
+    }
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        new ModbusClient().read(1, 101, 2);
+    }
+
+    public void read(int unitId, int address, int quality) throws InterruptedException, ExecutionException {
+        CompletableFuture<ReadHoldingRegistersResponse> future = master.sendRequest(
+            new ReadHoldingRegistersRequest(address, quality), unitId);
+        ReadHoldingRegistersResponse response = future.get();
+        ByteBuf byteBuf = response.getRegisters();
+        float value = byteBuf.readFloat();
+        System.out.println("时间" + System.currentTimeMillis() + " 读取地址" + address + "的值" + value);
+    }
 }
--- com.diaosichengxuyuan.network.parent/com.diaosichengxuyuan.network.protocal/src/main/java/com/diaosichengxuyuan/network/protocal/modbus/ModbusServer.java
+++ com.diaosichengxuyuan.network.parent/com.diaosichengxuyuan.network.protocal/src/main/java/com/diaosichengxuyuan/network/protocal/modbus/ModbusServer.java
@@ -1,9 +1,48 @@
 package com.diaosichengxuyuan.network.protocal.modbus;
 
+import com.digitalpetri.modbus.requests.ReadHoldingRegistersRequest;
+import com.digitalpetri.modbus.responses.ReadHoldingRegistersResponse;
+import com.digitalpetri.modbus.slave.ModbusTcpSlave;
+import com.digitalpetri.modbus.slave.ModbusTcpSlaveConfig;
+import com.digitalpetri.modbus.slave.ServiceRequestHandler;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.util.ReferenceCountUtil;
+
+import java.util.concurrent.ExecutionException;
+
 /**
  * @author liuhaipeng
  * @date 2018/11/18
  */
 public class ModbusServer {
+    private final ModbusTcpSlave slave = new ModbusTcpSlave(new ModbusTcpSlaveConfig.Builder().build());
 
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        new ModbusServer().start();
+        System.out.println("启动成功");
+    }
+
+    public void start() throws ExecutionException, InterruptedException {
+        slave.setRequestHandler(new ServiceRequestHandler() {
+            @Override
+            public void onReadHoldingRegisters(
+                ServiceRequest<ReadHoldingRegistersRequest, ReadHoldingRegistersResponse> service) {
+
+                ReadHoldingRegistersRequest request = service.getRequest();
+                int quantity = request.getQuantity();
+                int address = request.getAddress();
+                ByteBuf byteBuf = Unpooled.buffer();
+                float data = 76.54F;
+                for(int i = 0; i < quantity; i += 2) {
+                    byteBuf.writeFloat(data);
+                }
+                System.out.println(String
+                    .format("时间%s 接收到地址%s要读取%s个线圈的数据 给出数据：%s", System.currentTimeMillis(), address, quantity, data));
+                service.sendResponse(new ReadHoldingRegistersResponse(byteBuf));
+                ReferenceCountUtil.release(request);
+            }
+        });
+        slave.bind("localhost", 50200).get();
+    }
 }
