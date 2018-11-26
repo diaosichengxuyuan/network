6601049467277f90547501f3e09f7f304ff07592
“liuhaipeng” <>
2018-11-26 22:27:13 +0800

    开源协议

--- com.diaosichengxuyuan.network.parent/com.diaosichengxuyuan.network.protocal/src/main/java/com/diaosichengxuyuan/network/protocal/bacnet/BacnetClient.java
+++ com.diaosichengxuyuan.network.parent/com.diaosichengxuyuan.network.protocal/src/main/java/com/diaosichengxuyuan/network/protocal/bacnet/BacnetClient.java
@@ -21,40 +21,7 @@ import com.serotonin.bacnet4j.type.primitive.ObjectIdentifier;
  * @author liuhaipeng
  * @date 2018/9/26
  */
-public class BacnetClient {public static void main(String[] args) throws Exception {
-    IpNetwork ipNetwork = new IpNetworkBuilder().withBroadcast("30.1.255.255", 16).withLocalNetworkNumber(1)
-        .withPort(47808).build();
-    DefaultTransport transport = new DefaultTransport(ipNetwork);
-    LocalDevice localDevice = new LocalDevice(1, transport);
-    localDevice.getEventHandler().addListener(new DeviceEventAdapter() {
-        @Override
-        public void iAmReceived(final RemoteDevice d) {
-            System.out.println("收到：" + d);
-        }
-    });
-    localDevice.initialize();
-
-    Address address = new Address(1, IpNetworkUtils.toOctetString("30.1.224.30", 47808));
-    ObjectIdentifier objectIdentifier = new ObjectIdentifier(ObjectType.device, 1);
-    ReadPropertyRequest readPropertyRequest = new ReadPropertyRequest(objectIdentifier,
-        PropertyIdentifier.propertyList);
-    localDevice.send(address, readPropertyRequest, new ResponseConsumer() {
-        @Override
-        public void success(AcknowledgementService ack) {
-            System.out.println("成功响应" + ack);
-        }
-
-        @Override
-        public void fail(AckAPDU ack) {
-            System.out.println("失败响应：" + ack);
-        }
-
-        @Override
-        public void ex(BACnetException e) {
-            System.out.println("异常响应：" + e);
-        }
-    });
-}
+public class BacnetClient {
 
     public static void main(String[] args) throws Exception {
         IpNetwork ipNetwork = new IpNetworkBuilder().withBroadcast("localhost", 16).withLocalNetworkNumber(1).withPort(
